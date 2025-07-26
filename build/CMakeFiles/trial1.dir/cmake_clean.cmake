bc373fd6d83562b5c67375906f2c0619e1be9f6b
sezgin-karapinar <karapinarsezgin@hotmail.com>
2025-07-26 19:20:55 +0300

    First commit: project structure

---
+++ build/CMakeFiles/trial1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trial1.dir/src/main.cpp.o"
+  "CMakeFiles/trial1.dir/src/main.cpp.o.d"
+  "trial1"
+  "trial1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trial1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/trial1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for trial1.
+# This may be replaced when dependencies are built.
